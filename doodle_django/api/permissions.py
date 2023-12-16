c672c6bf3e219001700e18f363289cc60614e389
uhth <vincenzorizzomy@gmail.com>
2023-12-16 23:50:55 +0100

    Backend Fixes

--- doodle_django/api/permissions.py
+++ doodle_django/api/permissions.py
@@ -12,15 +12,30 @@ class MeetingPermissions(BasePermission):
 
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ['create', 'update', 'partial_update', 'destroy', 'retrieve']:
+        passcode = request.data.get("passcode", request.query_params.get("passcode"))
+        link_token = request.data.get("link_token", request.query_params.get("link_token"))
+        if view.action in ['update', 'partial_update', 'destroy']:
             if obj.user is not None:
                 return request.user == obj.user
             else:
-                return obj.passcode == request.data.get('passcode')
-        elif view.action == 'list':
-            return request.user and request.user.is_superuser
-        return False
-
+                return obj.passcode == passcode
+            return False
+        if view.action == 'retrieve':
+            if obj.user is not None and request.user == obj.user:
+                return True
+            if obj.passcode == passcode:
+                return True
+            schedule_poll = None
+            try:
+                schedule_poll = SchedulePoll.objects.get(meeting=obj)
+                schedule_poll_link = SchedulePollLink.objects.get(schedule_poll=schedule_poll)
+                if link_token is not None:
+                    link_token = uuid.UUID(link_token)
+                    return link_token == schedule_poll_link.token
+            except (SchedulePoll.DoesNotExist, SchedulePollLink.DoesNotExist):
+                return False
+            return False
+        return True
 
 class VotePermissions(BasePermission):
     def has_permission(self, request, view):
