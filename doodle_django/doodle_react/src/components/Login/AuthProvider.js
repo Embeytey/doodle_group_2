9256e21f9adf9e5be4a476ef282bbc19efc788b3
uhth <vincenzorizzomy@gmail.com>
2023-12-18 21:56:11 +0100

    Saved login info in localstorage to persist on F5

--- doodle_django/doodle_react/src/components/Login/AuthProvider.js
+++ doodle_django/doodle_react/src/components/Login/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
@@ -7,20 +7,35 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const isAuthState = localStorage.getItem(isAuthenticated);
+    const userState = localStorage.getItem(user);
+    if (isAuthState) {
+      setIsAuthenticated(JSON.parse(isAuthState));
+    }
+    if (userState) {
+      setUser(userState);
+    }
+
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', JSON.stringify(isAuthenticated));
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
   const login = (key) => {
     axios.defaults.headers.common["Authorization"] = `Token ${key}`;
     axios.defaults.headers.common["Content-Type"] = "application/json";
     setIsAuthenticated(true);
 
-    console.log("Request Headers:", axios.defaults.headers);
-
     axios
       .get(`http://127.0.0.1:8000/api/user-info/`)
       .then((response) => {
         setUserDetail(response.data);
-        console.log("USER INFO OK!");
-        const headersObject = response.headers.toJSON();
-        console.log("Headers:", headersObject);
       })
       .catch((error) => {
         console.log(error);
